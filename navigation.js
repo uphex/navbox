4007cadf0aa7460f26f246f994186c49cb8f07a4
sashee <gsashee@gmail.com>
2013-10-04 20:35:05 +0200

    refactor, first click initiates a search, rows without logos occupy at least 40 px height

--- navigation.js
+++ navigation.js
@@ -1,116 +1,122 @@
-function changed(element,maxresults){
-    //For empty text, no result will be shown
-    if(element.find('input').val()===""){
-        $('#navigationpanel').hide();
-    }else{
-        element.find(".ajax-loader").show();
-        $.get('search.json','q='+encodeURIComponent(element.find('input').val()),function(result){
-            element.find(".ajax-loader").hide();
-            //If the navigation panel is not present, create it
-            if($('#navigationpanel').length===0){
-                $('<div id="navigationpanel"/>').appendTo($(document.body));
-            }
-            
-            //If the input has lost focus since the AJAX was initiated, then no result is shown
-            if(element.find('input').is(":focus")){
-            
-                var navigationpanel=$('#navigationpanel');
-                navigationpanel.show();
-                element.find('.closetext').show();
-                navigationpanel.empty();
-                navigationpanel.css('top',element.find('input')[0].getBoundingClientRect().bottom+'px');
-                navigationpanel.css('left',element.find('input')[0].getBoundingClientRect().left+'px');
-                
-                var res;
-                if (typeof result === 'string') {
-                    res=JSON.parse(result);
-                } else {
-                    res=result;
-                }
+function changed(element,maxresults,value){
+    element.find(".ajax-loader").show();
+    $.get('search.json','q='+encodeURIComponent(value),function(result){
+        element.find(".ajax-loader").hide();
+        //If the navigation panel is not present, create it
+        if($('#navigation-panel').length===0){
+            $('<div id="navigation-panel"/>').appendTo($(document.body));
+        }
 
-                if(res.results.length===0){
-                    $('<div class="noresults">No results</div>').appendTo(navigationpanel);
-                }
-                
-                var groupTable=$('<tbody/>').appendTo($('<table/>').appendTo(navigationpanel));
-                var elementsGrouped=_.groupBy(_.first(res.results,maxresults),"type");
-                _.each(_.keys(elementsGrouped),function(key,index,list){
-                    //It indicates that this is the first row for the group, thus the group name should be added too
-                    var first=true;
-                    _.each(elementsGrouped[key],function(element){
-                        var row=$('<tr class="result"></tr>').appendTo(groupTable);
-                        $('<td>'+(first?key:'')+'</td>').appendTo(row);
-                        $('<td>'+(element.logo?'<img src="'+element.logo.src+'"/>':'')+'</td>').appendTo(row);
-                        $('<td>'+element.name+'</td>').appendTo(row);
-                        if(element.summary){
-                            if(element.summary.type=="link"){
-                                $('<td><a href="'+element.summary.src+'">'+element.summary.value+'</a></td>').appendTo(row);
-                            }else{
-                                $('<td><div class="healthbar"><div class="healthbar_inner" style="height:100%;width:'+(element.summary.value*100)+'%;background-color:'+(element.summary.status=="good"?'green':element.summary.status=="warning"?'orange':'red')+'"/></div></td>').appendTo(row);
-                            }
-                        }else{
-                            $('<td/>').appendTo(row);
-                        }
-                        
-                        //Hover handling, :hover is not efficient, as not all tds are needed to be shaded
-                        row.find('td:gt(0)').hover(function(){
-                            row.find('td:gt(0)').addClass('hover');
-                        },function(){
-                            row.find('td:gt(0)').removeClass('hover');
-                        });
-                        
-                        //Clicking on an anchor should navigate to the anchor's href
-                        row.find('td:gt(0)').click(function(a){
-                            if($(a.toElement).prop("tagName").toUpperCase()!="A"){
-                                window.location.href=element.src;
-                            }
-                        });
-                        first=false;
-                    });
-                    
-                    //After the last group, no separator is needed
-                    if(index!=list.length-1){
-                        $('<tr class=""><td colspan="4"><hr/></td>></tr>').appendTo(groupTable);
-                    }
-                });
-                
-                if(res.results.length>maxresults){
-                    $('<a class="seemore" href="search?q='+encodeURIComponent(element.find('input').val())+'">See more...</a>').appendTo(navigationpanel);
-                }
+        //If the input has lost focus since the AJAX was initiated, then no result is shown
+        if(element.find('input').is(":focus")){
+
+            var navigation_panel=$('#navigation-panel');
+            navigation_panel.show();
+            element.find('.close-text').show();
+            navigation_panel.empty();
+            navigation_panel.css('top',element.find('input')[0].getBoundingClientRect().bottom+'px');
+            navigation_panel.css('left',element.find('input')[0].getBoundingClientRect().left+'px');
+
+            var res;
+            if (typeof result === 'string') {
+                res=JSON.parse(result);
+            } else {
+                res=result;
             }
-        });
-    }
+
+            if(res.results.length===0){
+                $('<div class="no-results">No results</div>').appendTo(navigation_panel);
+            }
+
+            var group_table=$('<tbody/>').appendTo($('<table/>').appendTo(navigation_panel));
+            var elements_grouped=_.groupBy(_.first(res.results,maxresults),"type");
+            _.each(_.keys(elements_grouped),function(key,index,list){
+                //It indicates that this is the first row for the group, thus the group name should be added too
+                var first=true;
+                _.each(elements_grouped[key],function(element){
+                    var row=$('<tr class="result"></tr>').appendTo(group_table);
+                    $('<td>'+(first?key:'')+'</td>').appendTo(row);
+                    $('<td>'+(element.logo?'<img src="'+element.logo.src+'"/>':'<div class="image-placeholder"/>')+'</td>').appendTo(row);
+                    $('<td>'+element.name+'</td>').appendTo(row);
+                    if(element.summary){
+                        if(element.summary.type=="link"){
+                            $('<td><a href="'+element.summary.src+'">'+element.summary.value+'</a></td>').appendTo(row);
+                        }else{
+                            $('<td><div class="health-bar"><div class="health-bar-inner" style="height:100%;width:'+(element.summary.value*100)+'%;background-color:'+(element.summary.status=="good"?'green':element.summary.status=="warning"?'orange':'red')+'"/></div></td>').appendTo(row);
+                        }
+                    }else{
+                        $('<td/>').appendTo(row);
+                    }
+
+                    //Hover handling, :hover is not efficient, as not all tds are needed to be shaded
+                    row.find('td:gt(0)').hover(function(){
+                        row.find('td:gt(0)').addClass('hover');
+                    },function(){
+                        row.find('td:gt(0)').removeClass('hover');
+                    });
+
+                    //Clicking on an anchor should navigate to the anchor's href
+                    row.find('td:gt(0)').click(function(a){
+                        if($(a.toElement).prop("tagName").toUpperCase()!="A"){
+                            window.location.href=element.src;
+                        }
+                    });
+                    first=false;
+                });
+
+                //After the last group, no separator is needed
+                if(index!=list.length-1){
+                    $('<tr><td colspan="4"><hr/></td>></tr>').appendTo(group_table);
+                }
+            });
+
+            if(res.results.length>maxresults){
+                $('<a class="see-more" href="search?q='+encodeURIComponent(value)+'">See more...</a>').appendTo(navigation_panel);
+            }
+        }
+    });
 }
-    
+
+/**
+ *
+ * @param element The element to put the navbox to
+ * @param def The default string displayed
+ * @param maxresults The maximum results to show in the results
+ */
 function navbox(element,def,maxresults){
     element.addClass('navigation');
     //Span before clicked
-    var span=$('<div class="placeholder"><img src="search.png" class="searchicon"/><span>'+def+'</span><img src="select.png" class="selecticon"/></div>');
+    var placeholder=$('<div class="placeholder"><img src="search.png" class="search-icon"/><span>'+def+'</span><img src="select.png" class="select-icon"/></div>');
     //Input after clicked
     var input=$('<input type="text" onclick="select()">').appendTo(element).hide();
     
     $('<img src="ajax-loader.gif" class="ajax-loader"/>').appendTo(element).hide();
-    var closetext=$('<span class="closetext">X</span>').appendTo(element).hide();
+    var closetext=$('<span class="close-text">X</span>').appendTo(element).hide();
         
-    span.click(function(){
+    placeholder.click(function(){
         //span.hide();
-        span.css("color", "transparent");
+        placeholder.css("color", "transparent");
         input.width((element.width()-30)+'px');
         input.show();
-        //For debounce
+        /** For debounce */
         var timer;
         input.bind('input',function(){
             if(timer) clearTimeout(timer);
-            timer = setTimeout(function(){changed(element,maxresults);}, 300);
+            timer = setTimeout(function(){
+                if(element.find('input').val()===""){
+                    $('#navigation-panel').hide();
+                }else{
+                    changed(element,maxresults,element.find('input').val());
+                }
+            }, 300);
         });
         input.val(def);
 
         var hidenavigation=function(){
-            $('#navigationpanel').hide();
+            $('#navigation-panel').hide();
             input.val(def);
             closetext.hide();
-            //span.show();
-            span.removeAttr("style");
+            placeholder.removeAttr("style");
             input.hide();
         };
         
@@ -127,8 +133,10 @@ function navbox(element,def,maxresults){
         });
         
         input.select();
+
+        changed(element,maxresults,"");
     });
-    span.appendTo(element);
+    placeholder.appendTo(element);
     
     
 }
